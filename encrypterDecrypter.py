78b64f22aa786e2bd9cd1e98bfd225eecc5a3627
Coder0511 <mdelgado.2c.1516@gmail.com>
2022-09-27 19:11:57 +0200

    init

---
+++ encrypterDecrypter.py
@@ -0,0 +1,64 @@
+#BEGINNER LEVEL
+
+
+#Program prepared to encrypt or decrypt a given message (basic version).
+
+import os
+
+#Function:
+def checkOption(option):
+    isDigit = False
+    while isDigit == False:
+        if option.isdigit() == True:
+            option = int(option)
+            return option
+        else:
+            print("Please try again.\n")
+            print("Choose an option:")
+            print("1. Encrypt")
+            print("2. Decrypt\n")
+            print("Option: ", end="")
+            option = input()
+
+#Main code:
+
+print("Greetings, this is an encrypting and decrypting program, feel free to use it whenever you want.\n")
+print("Choose an option: ")
+print("1. Encrypt")
+print("2. Decrypt\n")
+print("Option: ", end="")
+choice = input()
+
+option = checkOption(choice)
+
+while ((option > 2) or (option < 1)):
+    print("Please try again.\n")
+    print("Choose an option:")
+    print("1. Encrypt")
+    print("2. Decrypt\n")
+    print("Option: ", end="")
+    choice = input()
+    option = checkOption(choice)
+
+print("Paste your text here: ", end="")
+text = input()
+output = ""
+    
+if (option == 1):
+    for i in text:
+        if (i == " "):
+            output += i
+        else:
+            unicode = ord(i)
+            output += chr(unicode + 7)
+
+else:
+    for i in text:
+        if (i == " "):
+            output += i
+        else:
+            unicode = ord(i)
+            output += chr(unicode - 7)
+            
+print(output)
+os.system("pause")
